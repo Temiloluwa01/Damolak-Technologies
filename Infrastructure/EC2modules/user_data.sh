3c91529d0f2a774a5c38680a19e01bac7b2a1400
Temi <mac@Temis-MacBook-Air.local>
2026-05-11 17:33:21 +0100

    Terraform Infrastructure Pipeline

--- Infrastructure/EC2modules/user_data.sh
+++ Infrastructure/EC2modules/user_data.sh
@@ -17,10 +17,6 @@ sudo apt-get install -y \
 # Add Docker GPG key
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 
-# Add Docker repository
-echo \
-  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 # Update package index
 sudo apt-get update
